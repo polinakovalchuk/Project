e27b051dc5778ab2d9adc685c03e760cb64ab1e9
polinakovalchuk <19fi.p.kovalchuk@std.npu.edu.ua>
2020-05-27 22:30:28 +0300

    Add task0104

---
+++ src/main/java/ua/edu/npu/lab01/task04/App.java
@@ -0,0 +1,10 @@
+package ua.edu.npu.lab01.task04;
+
+public class App {
+    public static void main(String[] args) {
+        StringArray array = new StringArray();
+        array.setStringsNumber(4);
+        array.inputStrings();
+        array.outputSpecial();
+    }
+}
