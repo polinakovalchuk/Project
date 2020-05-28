cb5230adb1250370b8e22cc6488568288c3e6f6c
polinakovalchuk <19fi.p.kovalchuk@std.npu.edu.ua>
2020-05-28 15:59:01 +0300

    Add task0203

---
+++ src/main/java/ua/edu/npu/lab02/task03/App.java
@@ -0,0 +1,10 @@
+package ua.edu.npu.lab02.task03;
+
+public class App {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog("Марго", 5);
+        dog1.dogInfo();
+        dog2.dogInfo();
+    }
+}
