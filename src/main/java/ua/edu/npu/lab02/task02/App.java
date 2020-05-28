1f083e1ed50da17f47257547d45d11eec2e3b8f7
polinakovalchuk <19fi.p.kovalchuk@std.npu.edu.ua>
2020-05-28 15:48:25 +0300

    Add task0202

---
+++ src/main/java/ua/edu/npu/lab02/task02/App.java
@@ -0,0 +1,10 @@
+package ua.edu.npu.lab02.task02;
+
+public class App {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Джек", 2);
+        Dog dog2 = new Dog("Барон", 9);
+        dog1.voice();
+        dog2.voice();
+    }
+}
